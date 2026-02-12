7f816b6c6886583c5b194aee893a2a648736ea51
qikun <qikun001@e.ntu.edu.sg>
2026-02-12 21:20:41 +0800

    Created bash scripts for the split-data posteriors

--- Experiments/Bike_Sharing/stan_generate_samples.sh
+++ Experiments/Bike_Sharing/stan_generate_samples.sh
@@ -10,7 +10,7 @@ num_samples_split=10000000
 num_samples_split_evaluation=1000000
 
 # generate samples from the full-data posterior (regarded as the true barycenter)
-#$stan_model_path sample num_warmup=$num_warmup num_samples=$num_samples_full  data file=$input_data_dir/data_full.json output file=$output_data_dir/posterior_full.csv random seed=9000
+$stan_model_path sample num_warmup=$num_warmup num_samples=$num_samples_full  data file=$input_data_dir/data_full.json output file=$output_data_dir/posterior_full.csv random seed=9000
 
 # generate samples from the split-data posteriors (regarded as the input measures)
 $stan_model_path sample num_warmup=$num_warmup num_samples=$num_samples_split  data file=$input_data_dir/data_split_0.json output file=$output_data_dir/posterior_split_0.csv random seed=1000
---
+++ Experiments/Bike_Sharing/stan_generate_samples_split_4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+stan_model_path=./Experiments/Bike_Sharing/bike_sharing_PoissonGLM
+input_data_dir=../../WB_data/Bike_Sharing/stan
+output_data_dir=../../WB_data/Bike_Sharing/generated_samples
+
+num_warmup=10000
+num_samples_full=1000000
+num_samples_split=10000000
+num_samples_split_evaluation=1000000
+
+# generate samples from the split-data posteriors (regarded as the input measures)
+$stan_model_path sample num_warmup=$num_warmup num_samples=$num_samples_split  data file=$input_data_dir/data_split_4.json output file=$output_data_dir/posterior_split_4.csv random seed=1004
+
+# generate samples from the split_data posteriors for evaluation (when computing V-values)
+$stan_model_path sample num_warmup=$num_warmup num_samples=$num_samples_split_evaluation  data file=$input_data_dir/data_split_4.json output file=$output_data_dir/posterior_for_evaluation_split_4.csv random seed=11004
