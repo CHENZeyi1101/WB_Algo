1ac6c6698936b66d5e7cf6861545d9728edffa02
qikun <qikun001@e.ntu.edu.sg>
2026-02-12 20:54:07 +0800

    Update stan_compile.sh

--- Experiments/Bike_Sharing/stan_compile.sh
+++ Experiments/Bike_Sharing/stan_compile.sh
@@ -1,7 +1,12 @@
-#!/bin/bashs
+#!/bin/bash
+
 # cmdstan installation directory
 cmdstan_dir=~/Development/cmdstan
+
 stan_model_path=./Experiments/Bike_Sharing/bike_sharing_PoissonGLM
+
 pwd=$(pwd)
+
 cd $cmdstan_dir
+
 make $pwd/$stan_model_path
