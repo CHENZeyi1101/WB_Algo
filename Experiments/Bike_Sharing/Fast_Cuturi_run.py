351cc35687f8e51df46de6374fcabbdf5ee4896b
qikun <qikun001@e.ntu.edu.sg>
2026-02-14 15:56:36 +0800

    Added stochastic FP and Fast_Cuturi for bike sharing

--- Experiments/Bike_Sharing/Fast_Cuturi_run.py
+++ Experiments/Bike_Sharing/Fast_Cuturi_run.py
@@ -1,114 +1,100 @@
 import numpy as np
-import ot
-# from .posterior_sampler import *
-from Experiments.Bike_Sharing.metrics_to_compare import *
 import json, os
+from pathlib import Path
+
 from Algorithms.Fast_Cuturi.free_support_WB import w2_barycenter_free_support_from_samples
+from Algorithms.data_manage import *
+from Experiments.metrics_to_compare import evaluate_MC
 from Experiments.CSV_read import *
 
 if __name__ == "__main__":
-    dim = 9
+    Cfg_PATH = Path(__file__).parent / "cfg.json"
+    with open(Cfg_PATH, "r") as f:
+        cfg_dict = json.load(f)
+
+    params = cfg_dict["params_posterior_aggregation_dim9"]
+
+    samples_dir = cfg_dict['samples_dir']
+
+    # assert existence
+    assert os.path.exists(samples_dir), f"Instance directory {samples_dir} does not exist."
+
+    num_measures = params["num_measures"]
+    MC_size = params["MC_size"]
+    eval_num_samples = params["eval_num_samples"]
+    csv_skip_rows = params["csv_skip_rows"]
+    csv_cols_range = params["cvs_cols_range"]
+
+    # number of atoms to approximate the input measures
     num_samples = 10000
-    num_measures = 5
-    truncated_radius = 1000
-    multiplication_factor = 1
-    MC_size = 20
+    
+    # number of atoms in the discrete supports
     support_size = 10000
-    print("Setting up posterior samplers...")
 
-    csv_dir = f"../../WB_data/Bike_Sharing"
-    print("CSV directory exists.")
-    total_posterior_sampler = csv_posterior_sampler_BikeSharing(csv_dir=csv_dir, 
-                                                    num_measures=num_measures, 
-                                                    multiplication_factor=multiplication_factor, 
-                                                    type="full",
-                                                    usecols=range(7, 16),
-                                                    skiprows=52)
-    total_posterior_sampler.set_streamers()
-    print("Total posterior sampler set up.")
 
-    split_posterior_sampler = csv_posterior_sampler_BikeSharing(csv_dir, 
-                                                num_measures, 
-                                                multiplication_factor, 
-                                                type="split",
-                                                usecols=range(7, 16),
-                                                skiprows=52)
+    split_posterior_sampler = csv_posterior_sampler_BikeSharing(csv_dir = samples_dir, 
+                                                num_measures = num_measures, 
+                                                multiplication_factor = 1, 
+                                                type = "split",
+                                                usecols = csv_cols_range,
+                                                skiprows = csv_skip_rows)
     split_posterior_sampler.set_streamers()
-    print("Split posterior sampler set up.")
 
-    split_posterior_sampler_for_evaluation = csv_posterior_sampler_BikeSharing(csv_dir, 
-                                                num_measures, 
-                                                multiplication_factor, 
-                                                type="split",
-                                                usecols=range(7, 16),
-                                                skiprows=6000000) # adjust skiprows for the samples used for evaluation
-    split_posterior_sampler_for_evaluation.set_streamers()
-    print("Split posterior sampler for evaluation set up.")
+    eval_dir = cfg_dict['samples_for_evaluation_dir']
 
-    # posterior_csv_dir = f"../WB_data/Bike_Sharing"
-    # total_posterior_sampler = csv_posterior_sampler(csv_dir=posterior_csv_dir, num_measures=1, multiplication_factor=multiplication_factor, type="full")
-    # split_posterior_sampler = csv_posterior_sampler(csv_dir=posterior_csv_dir, num_measures=num_measures, multiplication_factor=multiplication_factor, type="split")
-
-    bary_sample_path = f"../../WB_Data/Bike_Sharing/bary_samples_collection/bary_samples_collection_dim{dim}_MCsize{MC_size}_numsamples10000.json"
+    bary_sample_path = f"{eval_dir}/bary_samples_collection.json"
     with open(bary_sample_path, 'r') as json_file:
         bary_samples_collection_loaded = json.load(json_file)
-    bary_samples_collection_loaded = {k: np.array(v) for k, v in bary_samples_collection_loaded.items()}
+    bary_samples_collection_loaded = {int(k): np.array(v) for k, v in bary_samples_collection_loaded.items()}
 
-    data_dir = f"../../WB_Data/Bike_Sharing/outputs/Fast_Cuturi_outputs/SupportSize{support_size}_NumSamples{num_samples}"
-    os.makedirs(data_dir, exist_ok=True)
-    V_values_dir = os.path.join(data_dir, "V_values")
-    W2_to_bary_dir = os.path.join(data_dir, "W2_to_bary")
+    input_sample_path = f"{eval_dir}/input_samples_collection.json"
+    with open(input_sample_path, 'r') as json_file:
+        input_samples_collection_loaded = json.load(json_file)
+    input_samples_collection_loaded = {int(k): {int(i): np.array(u) for i, u in v.items()}
+                                        for k, v in input_samples_collection_loaded.items()}
+
+    
+ 
+    outputs_dir = f"{cfg_dict['outputs_dir']}/Fast_Cuturi_outputs"
+    os.makedirs(outputs_dir, exist_ok=True)
+
+    V_values_dir = os.path.join(outputs_dir, "V_values")
+    W2_to_bary_dir = os.path.join(outputs_dir, "W2_to_bary")
     os.makedirs(V_values_dir, exist_ok=True)
     os.makedirs(W2_to_bary_dir, exist_ok=True)
 
-    V_values_path = os.path.join(V_values_dir, f"V_values.json")
-    W2_to_bary_path = os.path.join(W2_to_bary_dir, f"W2_to_bary.json")
+    input_samples_collection = split_posterior_sampler.sample(num_samples)
+    samples_list = [np.array(input_samples_collection[key]) for key in sorted(input_samples_collection.keys())]
+    approx_bary = w2_barycenter_free_support_from_samples(
+        samples_list,
+        k=support_size,
+        init="random",
+        numItermax=200,
+        verbose=True,
+        seed=8842,
+    )
 
-    V_values_list = []
-    W2_to_bary_list = []
-    for i in range(MC_size):
-        print(f"Computing barycenter sample {i+1}/{MC_size}...")
-        input_samples_collection = split_posterior_sampler.sample(num_samples)
-        samples_list = [np.array(input_samples_collection[key]) for key in sorted(input_samples_collection.keys())]
-        approx_bary = w2_barycenter_free_support_from_samples(
-            samples_list,
-            k=support_size,
-            init="random",
-            numItermax=200,
-            verbose=True,
-            seed=42,
-        )
-
-        input_samples_collection_for_evaluation = split_posterior_sampler_for_evaluation.sample(num_samples)
-        bary_samples = bary_samples_collection_loaded[str(i)]
-
-        # compute V-value
-        V_value = 0
-        for measure_index in range(num_measures):
-            input_samples = np.array(input_samples_collection_for_evaluation[measure_index])
-            V_value += W2_pot(input_samples, approx_bary)
-        V_value /= num_measures
-        V_values_list.append(V_value)
-        print(f"V-value for barycenter sample {i}: {V_value}")
-
-        # compute W2 to barycenter samples
-        W2_sq = W2_pot(approx_bary, bary_samples)
-        W2_to_bary_list.append(W2_sq)
-        print(f"W2 squared to barycenter samples for barycenter sample {i}: {W2_sq}")
-
-        with open(V_values_path, 'w') as json_file:
-            json.dump(V_values_list, json_file) 
-
-        with open(W2_to_bary_path, 'w') as json_file:
-            json.dump(W2_to_bary_list, json_file)
-
+    # Evaluation
+    approx_bary_it = [approx_bary for _ in range(MC_size)]
+    input_measure_samples_collection_it = [{k : input_samples_collection_loaded[i][k][:eval_num_samples] for k in range(num_measures)} for i in range(MC_size)]
+    true_bary_samples_it = [bary_samples_collection_loaded[i][:eval_num_samples] for i in range(MC_size)]
     
+    V_values_list, W2_to_bary_list = evaluate_MC(approx_bary_it, 
+                                                 input_measure_samples_collection_it, 
+                                                 true_bary_samples_it, 
+                                                 MC_size = MC_size, 
+                                                 num_parallel_process = 5, 
+                                                 pbar_text = "Evaluation of Fast_Cuturi")
 
-   
+    # save V-values and W2_to_bary values
+    V_values_dict = {
+        "mean": np.mean(V_values_list),
+        "std": np.std(V_values_list),
+        "values": V_values_list}
+    save_json(V_values_dict, V_values_dir, "V_values.json")
 
-
-
-
-
-
-    
+    W2_to_bary_dict = {
+        "mean": np.mean(W2_to_bary_list),
+        "std": np.std(W2_to_bary_list),
+        "values": W2_to_bary_list}
+    save_json(W2_to_bary_dict, W2_to_bary_dir, "W2_to_bary.json")
--- Experiments/Bike_Sharing/stochastic_FP_run.py
+++ Experiments/Bike_Sharing/stochastic_FP_run.py
@@ -1,72 +1,88 @@
-from Algorithms.Stochastic_FP.entropic_iterative_scheme import *
+import numpy as np
+import json, os
+from pathlib import Path
+
 from Algorithms.data_manage import *
-from Experiments.Bike_Sharing.visualize_posteriors import *
+from Algorithms.Stochastic_FP.entropic_iterative_scheme import entropic_iterative_scheme
+from Experiments.metrics_to_compare import evaluate_MC
 from Experiments.CSV_read import *
 
 if __name__ == "__main__":
-    dim = 9
-    num_samples = 10000
-    num_measures = 5
-    truncated_radius = 1000
-    multiplication_factor = 1
-    MC_size = 20
-    max_iter = 5
-    epsilon = 10 ** -8
-    warm_start = False
-    data_dir = f"../../WB_Data/Bike_Sharing/outputs/stochastic_FP_outputs"
-    os.makedirs(data_dir, exist_ok=True)
+    Cfg_PATH = Path(__file__).parent / "cfg.json"
+    with open(Cfg_PATH, "r") as f:
+        cfg_dict = json.load(f)
+
+    params = cfg_dict["params_posterior_aggregation_dim9"]
+
+    samples_dir = cfg_dict['samples_dir']
+
+    # assert existence
+    assert os.path.exists(samples_dir), f"Instance directory {samples_dir} does not exist."
+
+    num_measures = params["num_measures"]
+    eval_MC_size = params["MC_size"]
+    eval_num_samples = params["eval_num_samples"]
+    csv_skip_rows = params["csv_skip_rows"]
+    csv_cols_range = params["cvs_cols_range"]
+
+    num_iters = 10
+    rand_state = np.random.RandomState(seed = 88888)
+    init_method = {"type": "moment", "sample_size": 10000}
+    truncate_radius = params["truncated_radius"]
+    sample_size_scheme = [5000, 5000, 10000, 10000, 20000, 20000, 40000, 40000, 80000, 80000]
+    reg_param_scheme = [1e-8] * num_iters
+    # sinkhorn_impl = "ott"
+    # warm_start = {"type": "first-order"}
+
+    sinkhorn_impl = "geomloss"
+    warm_start = None
 
     ##### Set up the samplers for Bike Sharing data #####
-    csv_dir = f"../../WB_data/Bike_Sharing"
-    print("CSV directory exists.")
-    total_posterior_sampler = csv_posterior_sampler_BikeSharing(csv_dir=csv_dir, 
-                                                    num_measures=num_measures, 
-                                                    multiplication_factor=multiplication_factor, 
-                                                    type="full",
-                                                    usecols=range(7, 16),
-                                                    skiprows=52)
-    total_posterior_sampler.set_streamers()
-    print("Total posterior sampler set up.")
-    split_posterior_sampler = csv_posterior_sampler_BikeSharing(csv_dir, 
-                                                num_measures, 
-                                                multiplication_factor, 
-                                                type="split",
-                                                usecols=range(7, 16),
-                                                skiprows=52)
+    split_posterior_sampler = csv_posterior_sampler_BikeSharing(csv_dir = samples_dir, 
+                                                num_measures = num_measures, 
+                                                multiplication_factor = 1, 
+                                                type = "split",
+                                                usecols = csv_cols_range,
+                                                skiprows = csv_skip_rows)
     split_posterior_sampler.set_streamers()
-    print("Split posterior sampler set up.")
 
-    split_posterior_sampler_for_evaluation = csv_posterior_sampler_BikeSharing(csv_dir, 
-                                                num_measures, 
-                                                multiplication_factor, 
-                                                type="split",
-                                                usecols=range(7, 16),
-                                                skiprows=6000000) # adjust skiprows for the samples used for evaluation
-    split_posterior_sampler_for_evaluation.set_streamers()
-    print("Split posterior sampler for evaluation set up.")
+    ##### Set up the samples for evaluation #####
+    eval_dir = cfg_dict["samples_for_evaluation_dir"]
+
+    bary_sample_path = f"{eval_dir}/bary_samples_collection.json"
+    with open(bary_sample_path, 'r') as json_file:
+        bary_samples_collection_loaded = json.load(json_file)
+    bary_samples_collection_loaded = {int(k): np.array(v) for k, v in bary_samples_collection_loaded.items()}
+
+    input_sample_path = f"{eval_dir}/input_samples_collection.json"
+    with open(input_sample_path, 'r') as json_file:
+        input_samples_collection_loaded = json.load(json_file)
+    input_samples_collection_loaded = {int(k): {int(i): np.array(u) for i, u in v.items()}
+                                        for k, v in input_samples_collection_loaded.items()}
 
     
     ##### Set up the entropic iterative computer #####
-    entropic_iterative_computer = entropic_iterative_scheme(dim = dim, 
-                                                            num_measures = num_measures, 
-                                                            bary_sampler = total_posterior_sampler, 
-                                                            input_sampler = split_posterior_sampler,
-                                                            truncate_radius = truncated_radius)
+    entropic_iterative_computer = entropic_iterative_scheme(
+        dim = dim,
+        num_iters = num_iters,
+        input_sampler = split_posterior_sampler,
+        rand_state = rand_state,
+        init_method = init_method,
+        truncate_radius = truncate_radius,
+        sinkhorn_impl = sinkhorn_impl,
+        sample_size_scheme = sample_size_scheme,
+        reg_param_scheme = reg_param_scheme,
+        warm_start = warm_start,
+        bary_samples_collection = bary_samples_collection_loaded, 
+        input_samples_for_evaluation = input_samples_collection_loaded,
+        eval_num_samples = eval_num_samples,
+        eval_MC_size = eval_MC_size,
+        num_parallel = 5
+    )
     
-    ##### Load the barycenter samples (for MC comparison) #####
-    bary_sample_path = f"../../WB_Data/Bike_Sharing/bary_samples_collection/bary_samples_collection_dim{dim}_MCsize50_numsamples10000.json"
-    with open(bary_sample_path, 'r') as json_file:
-        bary_samples_collection_loaded = json.load(json_file)
-    bary_samples_collection_loaded = {k: np.array(v) for k, v in bary_samples_collection_loaded.items()}
 
+    outputs_dir = f"{cfg_dict['outputs_dir']}/stochastic_FP_outputs"
+    os.makedirs(outputs_dir, exist_ok=True)
+    
     ##### Run the stochastic FP algorithm with entropic OT map estimation #####
-    entropic_iterative_computer.converge(bary_samples_collection_loaded,
-                                        split_posterior_sampler_for_evaluation,
-                                        max_iter = max_iter,
-                                        num_samples = num_samples,
-                                        epsilon = epsilon,
-                                        MC_size = MC_size,
-                                        logger = {'sample_logger': None, 'map_logger': None},
-                                        data_dir = data_dir,
-                                        warm_start = warm_start
-                                        )
+    entropic_iterative_computer.converge(logger = {'sample_logger': None, 'map_logger': None}, data_dir = outputs_dir)
